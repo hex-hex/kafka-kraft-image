b769a2b51b3ba9294d2a75dd0bd61b6349b27bc5
hex <he.x.working@gmail.com>
2022-08-02 18:54:17 +1200

    map port

--- start.py
+++ start.py
@@ -1,7 +1,7 @@
-import socket
 import os
-import subprocess
 import shlex
+import socket
+import subprocess
 
 LOG_NAME = 'kraft-combined-logs'
 
@@ -9,6 +9,6 @@ local_ip = socket.gethostbyname(socket.gethostname())
 
 if LOG_NAME not in os.listdir('/tmp'):
     uuid = subprocess.check_output(shlex.split('/home/kafka/bin/kafka-storage.sh random-uuid')).decode().strip()
-    subprocess.call( shlex.split(f'/home/kafka/bin/kafka-storage.sh format -t {uuid} -c /home/kafka/config/kraft/server.properties'))
+    subprocess.call(shlex.split(f'/home/kafka/bin/kafka-storage.sh format -t {uuid} -c /home/kafka/config/kraft/server.properties'))
 
-subprocess.call( shlex.split("/home/kafka/bin/kafka-server-start.sh /home/kafka/config/kraft/server.properties"))
+subprocess.call(shlex.split("/home/kafka/bin/kafka-server-start.sh /home/kafka/config/kraft/server.properties"))

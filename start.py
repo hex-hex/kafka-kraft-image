f560432f4c8de139e12af8d20edddbadac7af43c
hex <he.x.working@gmail.com>
2022-09-30 16:05:39 +1300

    change host

--- start.py
+++ start.py
@@ -4,6 +4,14 @@ import socket
 import subprocess
 
 LOG_NAME = 'kraft-combined-logs'
+ADVERTISED_LISTENERS = os.environ.get('ADVERTISED_LISTENERS', 'kafka:9092')
+
+with open('/home/kafka/config/kraft/server.properties', 'r') as f:
+    config_file = f.read()
+
+with open('/home/kafka/config/kraft/server.properties', 'w') as f:
+    f.write(config_file.format(ADVERTISED_LISTENERS))
+
 
 if LOG_NAME not in os.listdir('/tmp'):
     uuid = subprocess.check_output(shlex.split('/home/kafka/bin/kafka-storage.sh random-uuid')).decode().strip()
